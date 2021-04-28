d0d20fc61173a7d610f0036b0f115390b1da5668
Hasan0734 <Jahidhasan2856@gmail.com>
2021-04-28 10:24:36 +0600

    null vs undefined clear

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let name;
+function add(num, num2){
+    console.log(num + num2);
+    return
+}
+
+
+function add(num, num2){
+    console.log(num, num2)
+}
+const result = add(13)
+
+const premik = {name: "jahid", phone: 01743782856}
+
+console.log(premik.gf);
+let fun = undefined;
+console.log(fun)
+
+const array = [20, 10, 25, 11];
+console.log(array[11]);
