65607cadc997238c642d297a630a92a5e4fb6e01
Hasan0734 <Jahidhasan2856@gmail.com>
2021-04-28 10:10:10 +0600

    true vs false

---
+++ trueFalse.js
@@ -0,0 +1,19 @@
+//falsy value
+// 0
+// ""
+//false
+// undefined         without define variable value
+// null
+// NaN
+//truethy value 
+//'0', ' ', [], true, {}
+// const age = 0;
+let name = {};
+
+
+if (name) {
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
